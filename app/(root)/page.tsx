6e373660e966a588b67e5b84249887ee185250ea
AliSabet81 <sabet.farshid1381@gmail.com>
2024-11-25 13:12:46 +0330

    home page heading

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -22,19 +22,6 @@ export default async function Home({
 
         <SearchForm query={query} />
       </section>
-      <section className="pink_container">
-        <h1 className="heading">
-          Pitch Your Startup, <br />
-          Connect With Entrepreneurs
-        </h1>
-
-        <p className="sub-heading !max-w-3xl">
-          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
-          Competitions.
-        </p>
-
-        <SearchForm query={query} />
-      </section>
     </>
   );
 }
